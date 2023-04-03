3eb8e76e5728af321eaec98cd3aa8d2492b427a0
rgandhre <47285565+rgandhre@users.noreply.github.com>
2023-04-02 21:52:20 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,8 +60,8 @@ streamlit.dataframe(my_data_row)
 
 #New section to add Text Entry Box
 fruit_choice = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('The user entered', fruit_choice)
+#streamlit.write('The user entered', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-streamlit.text("Thanks for adding" + fruit_choice)
+streamlit.text("Thanks for adding " + fruit_choice)
 
 
