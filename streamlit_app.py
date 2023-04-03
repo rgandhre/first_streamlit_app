d4c478576c4665d7515d9b42785355156f59bda3
rgandhre <47285565+rgandhre@users.noreply.github.com>
2023-04-02 21:12:52 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-import snowflake-connector
+import requirements.txt
 
 streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Menu')
